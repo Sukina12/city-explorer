61c30e1c60d7142fe73a7b1fef020fb289de4466
Sukina AbuHammad <sukina_89@yahoo.com>
2021-05-12 02:19:21 +0300

    finished

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
+import { CardColumns } from 'react-bootstrap';
 
 class Movie extends React.Component {
   render() {
     return (
-      this.props.movieData.map(data => {
-        console.log(data);
-        return (
-          <div>
-            <Card style={{ width: '100rem' }}>
-              <Card.Body>
+      <CardColumns>
+        {this.props.movieData.map(data => {
+          console.log(data);
+          return (
+            <div>
+              <Card style={{ width: '100rem' }} className="card">
+                <Card.Body>
 
-                <Card.Img style={{ width: '20rem', height: '20rem' }}
-                  src={`https://www.themoviedb.org/t/p/original/${data.img}`}
-                  alt={data.description}
-                />
+                  <Card.Img style={{ width: '20rem', height: '20rem' }}
+                    src={`https://www.themoviedb.org/t/p/original/${data.img}`}
+                    alt={data.description}
+                  />
 
-                <Card.Title>{data.title}</Card.Title>
-                <Card.Text className="cardDescription">
-                  Description: {data.description}
-                </Card.Text>
+                  <Card.Title>{data.title}</Card.Title>
+                  <Card.Text className="cardDescription" >
+                    Release Date: {data.releasedDate}
+                  </Card.Text>
+                  <Card.Text className="cardDescription" style={ {overflow: scroll} }>
+                    Description: {data.description}
+                  </Card.Text>
 
-              </Card.Body>
-            </Card>
-          </div>
-
-        );
-      })
+                </Card.Body>
+              </Card>
+              <br/>
+              <br/>
+            </div>
+          );
+        })
+        }
+      </CardColumns>
     );
   }
 }
